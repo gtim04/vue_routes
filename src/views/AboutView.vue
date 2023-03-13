91730f718a7fa28f468a6cafae9ad6eeff04ac36
gtim04 <gerald.timtiman@gmail.com>
2023-03-13 23:00:12 +0800

    end lesson routing, moving to fetching data

---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis quos dolore provident doloribus, cumque necessitatibus illum? Corrupti impedit autem in quo obcaecati quas molestias unde. Dignissimos, exercitationem excepturi quis neque dicta fuga eaque voluptatem pariatur consequatur dolor quam tenetur labore magnam ratione eligendi facere corporis accusantium similique, ipsum at? Aperiam quo laborum magni impedit veritatis. Molestias qui molestiae quo. Optio eius sunt officiis et minima amet earum, similique velit doloribus tempora, commodi autem dicta quis dolores nulla sapiente explicabo placeat atque nesciunt ratione cupiditate. Suscipit officia optio eveniet rerum esse? Repellat ab illum officiis, nesciunt dolorum modi deserunt at saepe?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur nobis nemo, laudantium dignissimos ipsum totam dicta ex dolorum porro veniam laborum corrupti, quaerat fugit natus mollitia debitis suscipit voluptatum error quasi, facere exercitationem? Enim hic quo ea corporis dolor obcaecati. Deserunt, animi quos corporis velit adipisci itaque doloremque quasi suscipit?</p>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, enim impedit. Minus suscipit repellendus asperiores officiis similique ratione vero, sapiente ipsum at culpa optio dolorem nulla unde eveniet libero, eligendi harum corrupti. Iste, unde quibusdam! Nihil distinctio maxime ullam iure, sapiente cumque labore exercitationem sequi sed dolorum ea soluta dolore recusandae excepturi laudantium libero consequuntur odio obcaecati reiciendis provident. Error voluptatem, ratione rerum aspernatur cupiditate repudiandae placeat nostrum id hic, excepturi ea corrupti, velit molestiae. Veritatis at architecto voluptate odit debitis laudantium, placeat aspernatur alias deleniti voluptas exercitationem laborum aliquam ipsa suscipit illum dicta eum vero ratione. Nesciunt, tempore saepe?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt repellendus corrupti recusandae molestias atque ea possimus esse odio doloribus? Suscipit quasi eos itaque, vitae fugit a perspiciatis vero iure quo pariatur necessitatibus fuga earum eveniet unde ratione totam est tempore reiciendis harum. Maiores dolores atque earum repellat. Assumenda magni ut dicta quas eius cupiditate expedita officiis. Quisquam nulla non, quia officia voluptas blanditiis temporibus modi tenetur, laborum, enim qui obcaecati! Tempore exercitationem sunt ducimus minus eos similique non dolores numquam distinctio maxime quidem magnam rem eveniet, aut, architecto fugiat ut magni quisquam velit. Quisquam, veritatis incidunt ipsa excepturi quo iure?</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
